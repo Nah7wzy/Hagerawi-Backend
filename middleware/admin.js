f2ed163eab360b0ce26d314851b2bc567bae8373
AbrishDew <dewtesfaye76@gmail.com>
2021-09-10 11:35:02 +0300

    fixing minor authentication errors

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,7 @@
+require('../models/models.js');
 module.exports = function (req, res, next) {
     // 401 unauthorized 
     // 403 forbidden 
-    if (!req.user.isAdmin) return res.status(403).send("Access denied.");
+    if (!req.isAdmin) return res.status(403).send("Access denied.");
     next();
 }
